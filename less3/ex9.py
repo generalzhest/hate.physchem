07cd24a4319c34014d32022c9f44fec202ccdf63
generalzhest <generalzhest@gmail.com>
2016-09-16 10:07:26 +0300

    lab3

--- less3/ex9.py
+++ less3/ex9.py
@@ -1,13 +1,15 @@
 import turtle as t
 import numpy as np
+
 n = 3
 for i in range(1, 11):
-    a = 2 * i * 10 * np.sin(360 / (2 * n))
-    for i in range (n):
+    a = 2 * i * 20 * np.sin((2 * np.pi) / (2 * n))
+    for i in range(n):
         t.forward(a)
         t.left(180 - (n - 2) * (180 / n))
-    t.right((180 - (n - 2) * (180 / n)) / 2)
+    t.right((180 - (n - 2) * (180 / n)) + ((n - 2) * 180) / (2 * n))
     t.penup()
-    t.forward(10)
+    t.forward(20)
+    t.left(180 - ((n - 1) * 180) / (2 * (n + 1)))
     t.pendown()
     n += 1
